7da5b29c6644ec96f66ffbe795dce4ea52acbc25
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-28 16:39:40 -0800

    Initialized Parse SDK

--- lab-insta-parse/AppDelegate.swift
+++ lab-insta-parse/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Charlie Hieger on 10/29/22.
 //
 
+import ParseSwift
 import UIKit
 
 // TODO: Pt 1 - Import Parse Swift
@@ -15,13 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        // TODO: Pt 1 - Initialize Parse SDK
+        // Pt 1 - Initialize Parse SDK
         // https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
-
-
-        // TODO: Pt 1: - Instantiate and save a test parse object to your server
-        // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L121
-
+        ParseSwift.initialize(applicationId: "dHzU8jIiubswHlgH5LuJssdGrXlmEvfi3sj2MRuD",
+                              clientKey: "KqaobGFReVtAPewkGaEAHjoJgq8l85C9D3a9Oibw",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
 
         return true
     }
@@ -40,8 +39,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
-
-// TODO: Pt 1 - Create Test Parse Object
-// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
-
-
